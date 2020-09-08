eb63866af235bd5b886840f070a83939275df843
StampoulisSpiros <Darkdream3r>
2020-09-08 19:41:32 +0200

    Build for netlify

---
+++ build/precache-manifest.51a57c5f21d737cc376c4cf1ebb7069d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ed40e0669bb04bff0beb719745f80e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b4a6fea590e7a6cbfdc",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4a6fea590e7a6cbfdc",
+    "url": "/static/js/main.bb7055c8.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
